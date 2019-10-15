84beac1e4613048a6eddc8316fcda12db88c16c6
Rixio Barrios <rixiobarrios.consulado@gmail.com>
2019-10-15 21:40:08 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,6 +4,6 @@ def meal_choice(veg1, veg2, protein="meat")
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-veg1(Carrots)
-veg2(Lentils)
+veg1(carrots)
+veg2(lentils)
 protein(chicken)
